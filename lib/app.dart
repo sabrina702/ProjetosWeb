173fb8e500df26c2dd38eecf5205ee49d344b02e
sabrinassgs101 <sabrinassgs101@gmail.com>
2025-10-29 17:44:52 +0000

    feat 1: Tela Inicial

---
+++ lib/app.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/presentation/pages/homePage.dart';
+import 'package:myapp/theme/colors.dart';
+import 'package:myapp/theme/text_styles.dart';
+
+class CuideSeMaisApp extends StatelessWidget {
+  const CuideSeMaisApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cuide-se Mais',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+        scaffoldBackgroundColor: AppColors.background,
+        appBarTheme: AppBarTheme(
+          backgroundColor: AppColors.primary,
+          titleTextStyle: AppTextStyles.appBarTitle,
+          iconTheme: const IconThemeData(color: Colors.white),
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF6C63FF); // Roxinho do topo e bottom bar
+  static const Color background = Color(0xFFF5F6FA); // Fundo suave
+  static const Color textPrimary = Color(0xFF333333);
+  static const Color card = Colors.white;
+  static const Color icon = Color(0xFF6C63FF);
+}
